1dc472f486774deaa1a136f8bfc5bbf00fef9dfc
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-12-16 09:20:09 -0600

    Update person_scan.py

--- person_scan.py
+++ person_scan.py
@@ -215,9 +215,9 @@ while True:
             cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 	    
 	    # Trigger GPIO pin when object detection
-	    if(int(scores[i]*100 >= 55):
-	        print("Person Detected: ", object_name)
-	        time.sleep(10)
+            if(int(scores[i]*100 >= 55):
+                print("Person Detected: ", object_name)
+                time.sleep(10)
 	       
     # Draw framerate in corner of frame
     cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
